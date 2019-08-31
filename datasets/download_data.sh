d428a775be388fcde54554e091f7b9901b6992c1
nschor <nschor@gmail.com>
2019-08-31 22:33:53 +0300

    download_data

--- datasets/download_data.sh
+++ datasets/download_data.sh
@@ -2,5 +2,3 @@
 wget https://shapenet.cs.stanford.edu/media/shapenetcore_partanno_segmentation_benchmark_v0.zip
 unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
-mkdir -p data
-mv shapenetcore_partanno_segmentation_benchmark_v0 data
